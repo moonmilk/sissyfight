3e8729e59798d31de3f9ea25407f901a807b8223
Ranjit Bhatnagar <ranjit@moonmilk.com>
2014-02-25 20:03:30 -0500

    client-game: adjusted position of name label and added translucent backing to make name more readable when over a large accessory

--- public/client/GameRoomPlayer.js
+++ public/client/GameRoomPlayer.js
@@ -69,10 +69,20 @@ var p = GameRoomPlayer.prototype = new createjs.Container();
 		// text color is the same as the 2nd color in uniform palette - convert it to css rgb form:
 		var textColor = 'rgb(' + config.colors.uniforms.vars[playerInfo.avatar.uniformcolor][1].join(',') + ')';
 		
+
+		// make a translucent cartouche behind nametag to make it easier to read
+		this.items.nametagCartouche = this.addChild(new createjs.Shape());
+
 		this.items.nametag = this.addChild(new createjs.Text(this.playerInfo.nickname, config.getFont('gamePlayerName'), textColor));
 		this.items.nametag.textAlign = 'center';
 		this.items.nametag.x = 43;
-		this.items.nametag.y = 80;
+		this.items.nametag.y = 75;
+		
+		var nameBounds = this.items.nametag.getBounds();
+		var halfWidth = Math.floor(nameBounds.width/2);
+		this.items.nametagCartouche.graphics
+			.beginFill('rgba(255,255,255,0.80)')
+			.drawRoundRect(43-halfWidth-2, 76, nameBounds.width+4, 11, 1);
 		
 		this.items.bubble = this.addChild(this.assets.bubble.clone());
 		this.items.bubble.x = 0;
