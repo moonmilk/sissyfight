8610cc2c87082c08c632ecc3a823366a8be04218
Ranjit Bhatnagar <ranjit@moonmilk.com>
2014-07-21 17:00:55 -0400

    got rid of a bunch of client console logging.

--- public/client/Comm.js
+++ public/client/Comm.js
@@ -20,22 +20,24 @@ var p = Comm.prototype;
 		
 		this.auth = auth;
 		this.sockjs = new SockJS(auth.endpoint);
+		
+		this.logging = false;
 
 		// when connection is established, attempt to log in
 		this.sockjs.onopen = function() {
-			console.log("Comm: connection open: " + comm.sockjs.protocol); 
+			if (this.logging) console.log("Comm: connection open: " + comm.sockjs.protocol); 
 			comm.writeEvent('login', {token:comm.auth.token, session:comm.auth.session});
 		}
 		
 		this.sockjs.onclose = function() {
 			// not sure what to do about this yet!
-			console.log('lost socket connection');
+			if (this.logging) console.log('lost socket connection');
 			alert('DRAT! Lost server connection - either server crashed or your internet did.  Please use the reload button to start over.')
 		}
 		
 		// decode incoming messages in {type, data} style for dispatch
 		this.sockjs.onmessage = function(e) {
-			console.log("Comm: received " + e.data);
+			if (this.logging) console.log("Comm: received " + e.data);
 			var message;
 			try {
 				message = JSON.parse(e.data);
@@ -54,7 +56,7 @@ var p = Comm.prototype;
 
 	// encode {type, data} style messages and transmit 
 	p.writeEvent = function(type, data) {
-		if (type != 'ping') console.log("Comm: sending " + JSON.stringify({type:type, data:data}));
+		if (type != 'ping') if (this.logging) console.log("Comm: sending " + JSON.stringify({type:type, data:data}));
 		this.sockjs.send(JSON.stringify({type:type, data:data}));
 	}	
 	
