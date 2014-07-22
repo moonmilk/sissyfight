moonmilk/sissyfight

commit 4fdcd60687a18017496d40617fd8d19b4948cbeb

set lolly / tattle counters to 0 in games without lolly or tattle

gameObjects/sfgame.js
@@ -258,6 +258,9 @@ SFGame.prototype.prepareGame = function() {
 		this.players[conn.user.id].id = conn.user.id;
 		this.players[conn.user.id].nickname= conn.user.nickname;
 		
+		if (this.custom.moves['lick']==0) this.players[conn.user.id].lollies = 0;
+		if (this.custom.moves['tattle']==0) this.players[conn.user.id].tattles = 0;
+		
 	}, this);
 }
 

commit dd13e64987b005826116391a848ea69587d9f1e9

fix nasty bug introduced in 4fdcd60

gameObjects/sfgame.js
@@ -258,8 +258,10 @@ SFGame.prototype.prepareGame = function() {
 		this.players[conn.user.id].id = conn.user.id;
 		this.players[conn.user.id].nickname= conn.user.nickname;
 		
-		if (this.custom.moves['lick']==0) this.players[conn.user.id].lollies = 0;
-		if (this.custom.moves['tattle']==0) this.players[conn.user.id].tattles = 0;
+		if (this.custom && this.custom.moves) {
+			if (this.custom.moves['lick']==0) this.players[conn.user.id].lollies = 0;
+			if (this.custom.moves['tattle']==0) this.players[conn.user.id].tattles = 0;
+		}
 		
 	}, this);
 }